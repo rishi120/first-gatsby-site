96a4763a2164296cac3e18ae41dae53027793764
rishi120 <r.baruah42@gmail.com>
2022-02-21 13:46:49 -0800

    added hover state using tailwind css classes

--- src/routes/navbar.js
+++ src/routes/navbar.js
@@ -7,7 +7,7 @@ export default function Navbar({ navMenu, active, handleNavLink }) {
       <h1 className='font-bold text-3xl'>Web Warrior</h1>
       <div className="inline-block text-right">
         {navMenu.map((menuItems, index) => {
-          return <Link to={menuItems.navPath} key={index} className={active === menuItems.navPath ? "active" + " inline-block ml-12 font-semibold text-sm pb-2" : "" + " inline-block ml-12 font-semibold text-sm pb-2"}>{menuItems.navTitle}</Link>
+          return <Link to={menuItems.navPath} key={index} className={active === menuItems.navPath ? "active" + " inline-block ml-12 font-semibold text-sm pb-2 transition-all hover:text-black" : "" + " inline-block ml-12 font-semibold text-sm pb-2 transition-all hover:text-black"}>{menuItems.navTitle}</Link>
         })}
       </div>
     </nav>

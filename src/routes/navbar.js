98a0d2f5b7d5cbe22fe6f6139aaa4074b3bc06a0
rishi120 <r.baruah42@gmail.com>
2022-02-21 18:11:50 -0800

    added a function to render the navmenu links and added the @apply directive from tailwind css.

--- src/routes/navbar.js
+++ src/routes/navbar.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import "../styles/global.css";
 
-export default function Navbar({ navMenu, active, handleNavLink }) {
+export default function Navbar({ navMenu, active }) {
+
+  const renderMenuLink = (menuItems, index) => {
+    return <Link to={menuItems.navPath} key={index} className={active === menuItems.navPath ? "active" + " navLink hover:text-black" : "" + " navLink hover:text-black"}>{menuItems.navTitle}</Link>
+  }
   return (
     <nav className='flex justify-between'>
       <h1 className='font-bold text-3xl'>Web Warrior</h1>
       <div className="inline-block text-right">
         {navMenu.map((menuItems, index) => {
-          return <Link to={menuItems.navPath} key={index} className={active === menuItems.navPath ? "active" + " inline-block ml-12 font-semibold text-sm pb-2 transition-all hover:text-black" : "" + " inline-block ml-12 font-semibold text-sm pb-2 transition-all hover:text-black"}>{menuItems.navTitle}</Link>
+          return renderMenuLink(menuItems, index)
         })}
       </div>
     </nav>
